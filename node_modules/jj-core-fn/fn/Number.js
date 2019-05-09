3192c5aa2a55ef74599a95ac6372de7b680e38af
Weiqing Ji <hzez1110@163.com>
2019-05-09 09:11:54 +0800

    update

--- node_modules/jj-core-fn/fn/Number.js
+++ node_modules/jj-core-fn/fn/Number.js
@@ -1,71 +1,33 @@
 class NumberFn {
     /**
      * 对非数字的内容，返回默认想展示的内容defaultVal || --
-     * @param {*} val 
-     * @param {*} defaultVal 
+     * @param {*} a 
+     * @param {*} b 
      */
-    n(val, defaultVal) {
-        if(/^[0-9]+.?[0-9]*$/.test(val)){
-            return val
-        }else{
-            return defaultVal ? defaultVal : 0
-        }
+    n(a, b) {
+        return /^[0-9]+.?[0-9]*$/.test(a) ? a : (b ? b : 0)
     }
     /**
      * 数值转换为两位数的金额
      * @param {*} rmb 为整数
      */
-    showMoney(rmb) {
-        rmb = rmb + "";
-        let numLength = rmb.length;
-        let rmb1;
-        let rmb2
-        if (numLength > 3) {
-            rmb1 = rmb.substring(0, numLength - 2);
-            rmb2 = rmb.substring(numLength - 2);
-            return rmb1 + "." + rmb2
-        } else if (numLength == 3) {
-            rmb1 = rmb.substring(0, 1);
-            rmb2 = rmb.substring(1);
-            return rmb1 + "." + rmb2
-        } else if (numLength == 2) {
-            return "0." + rmb;
-        } else if (numLength == 1) {
-            return "0.0" + rmb;
+    showMoney(a) {
+        a = String(a);
+        let l = a.length;
+        if (l > 2) {
+            return a.replace(/(\d{2})$/g,"\.$1")
+        } else if (l == 2) {
+            return "0." + a;
+        } else if (l == 1) {
+            return "0.0" + a;
         }
     }
     /**
-     * 金额的千位符显示
+     * 单位为元的金额的千位符显示
      * @param {*} rmb 为整数
      */
     showThousandMoney(rmb) {
-        rmb = rmb + "";
-        let numLength = rmb.length;
-        if (numLength > 5) {
-            let rmb1 = rmb.substring(0, numLength - 2);
-            let rmb2 = rmb.substring(numLength - 2);
-            rmb1 = rmb1.split("").reverse();
-            for (let i = 1; i < (numLength - 2); i++) {
-                if (i % 3 == 0) {
-                    rmb1[i] = rmb1[i] + ","
-                }
-            }
-            rmb1.reverse();
-            rmb1 = rmb1.join("");
-            return rmb1 + "." + rmb2;
-        } else if (numLength > 3) {
-            let rmb1 = rmb.substring(0, numLength - 2);
-            let rmb2 = rmb.substring(numLength - 2);
-            return rmb1 + "." + rmb2
-        } else if (numLength == 3) {
-            let rmb1 = rmb.substring(0, 1);
-            let rmb2 = rmb.substring(1);
-            return rmb1 + "." + rmb2
-        } else if (numLength == 2) {
-            return "0." + rmb;
-        } else if (numLength == 1) {
-            return "0.0" + rmb;
-        }
+        return parseFloat(rmb).toFixed(2).replace(/(\d{1,3})(?=(\d{3})+(?:\.))/g, "$1,")
     }
 }
 export const NumberClass = NumberFn
--- node_modules/jj-core-fn/index.js
+++ node_modules/jj-core-fn/index.js
@@ -1,3 +1,2 @@
 import Fn from './main.js'
-// console.log(new Fn().showThousandMoney(121100))
 export default new Fn()
